6a5cb8fb9691b4fd48ad6801cdd6f2fdfda6c275
Brandon Gibson <jab.gib@gmail.com>
2014-12-29 12:05:41 -0700

    Setup for "get" module. pik -M=get "http://google.com" is the same as pik "http://google.com"

--- pik.go
+++ pik.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
+
+var method string
+var args []string
 
 func main() {
 
-	var method = flag.String("M", "get", "The Http method to use. Defaults to 'get'")
+	method := flag.String("M", "get", "The Http method to use. Defaults to 'get'")
 
 	flag.Parse()
+	args = flag.Args()
 
-	fmt.Println(method)
-	fmt.Println(*method)
-	fmt.Println(flag.Args())
+	if strings.ToLower(*method) == "get" {
+		get()
+	}
+}
+
+func get() {
+
+	if len(args) != 1 {
+		log.Fatal("pik get requires 1 parameter (url)")
+	}
+
+	rs, err := http.Get(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rs.Body.Close() //TODO maybe should be in previous if else block
+
+	body, err2 := ioutil.ReadAll(rs.Body)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println(string(body))
 }
