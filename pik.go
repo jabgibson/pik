cbaaca8e8c1af61c485491b0ea51325a9fff6e0a
Brandon Gibson <jab.gib@gmail.com>
2015-01-02 16:09:24 -0700

    code update. A little of everything. Now that get functionality is stable.. I need to cleanup and structure the code.

--- pik.go
+++ pik.go
@@ -10,31 +10,52 @@ import (
 )
 
 var method string
+var doc string
+var body string
+var url string
+
 var args []string
+var result string
+
+
 
 func main() {
 
-	method := flag.String("M", "get", "The Http method to use. Defaults to 'get'")
+	method := flag.String("met", "get", "The Http method to use. Defaults to 'get'")
+	doc := flag.String("doc", "application/json", "The document type for the body (POST)")
+	body := flag.String("body", nil, "The Body for the POST method")
+	url := flag.String("url", nil, "The url for the HTTP action")
 
 	flag.Parse()
 	args = flag.Args()
 
+
+
 	if strings.ToLower(*method) == "get" {
-		get()
+		result = get()
 	} else if strings.ToLower(*method) == "post" {
-		post()
+		result = post()
 	}
+
+	if *run != "NONE" {
+		
+	} 
 }
 
-func get() {
+func get() string {
 
 	if len(args) != 1 {
 		log.Fatal("pik get requires 1 parameter (url)")
 	}
 
-	rs, err := http.Get(args[0])
-	if err != nil {
-		log.Fatal(err)
+
+	/** trys the user defined arg. If it doesn't work, attempts "http://" prepended to it */
+	rs, err1 := http.Get(args[0])
+	if err1 != nil {
+		rs, err1 = http.Get("http://" + args[0])
+		if err1 != nil {
+			log.Fatal(err1)
+		}		
 	}
 
 	defer rs.Body.Close() //TODO maybe should be in previous if else block
@@ -44,11 +65,11 @@ func get() {
 		log.Fatal(err2)
 	}
 
-	fmt.Println(string(body))
+	return string(body)
 }
 
 func post() {
 	if len(args) != 2 {
 		log.Fatal("pik post requires 2 parameters. Ex. ---> pik -M=post http://boom.com/insert \"{item1: \"item1\"}\" ")
-		}
+	}
 }
